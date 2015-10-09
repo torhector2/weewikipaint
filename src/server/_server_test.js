65319ad80f51dbf84de92a56db6f2278e9b1e902
torhector2 <torhector2@gmail.com>
2015-10-09 17:44:02 +0200

    Our first test-driven method

--- src/server/_server_test.js
+++ src/server/_server_test.js
@@ -1,6 +1,8 @@
 "use strict";
 
+var server = require("./server");
+
 exports.testNothing = function(test) {
-	test.ok(true, "hello");
+	test.equals(3, server.number(), "number");
 	test.done();
 };
--- src/server/server.js
+++ src/server/server.js
@@ -1,2 +1,7 @@
+"use strict";
 
 console.log("Hello, I'm a server. No, really. What?");
+
+exports.number = function() {
+	return 3;
+};
