a308c45d8d641863ea3d83def9a44497d5d4175a
torhector2 <torhector2@gmail.com>
2015-10-09 17:37:52 +0200

    Change test reporter to use minimal reporter

--- jakefile.js
+++ jakefile.js
@@ -20,7 +20,7 @@ task("lint", [], function() {
 
 desc("Test everything");
 task("test", [], function() {
-	var reporter = require("nodeunit").reporters["default"];
+	var reporter = require("nodeunit").reporters.minimal;
 	reporter.run(["src/server/_server_test.js"]);
 });
 
