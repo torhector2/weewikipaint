54b3d29b4921644798fccbd07d536adc07661297
torhector2 <torhector2@gmail.com>
2015-10-20 20:50:23 +0200

    Chapter 20

--- src/server/_server_test.js
+++ src/server/_server_test.js
@@ -1,59 +1,100 @@
-"use strict";
+(function() {
+	"use strict";
 
-var server = require("./server");
-var http = require("http");
-var fs = require("fs");
+	var server = require("./server");
+	var http = require("http");
+	var fs = require("fs");
+	var assert = require("assert"); //Por cómo trabaja nodeunit no tiene asserts en setUp y teardown así que utilizamos otra librería
 
-exports.test_serverReturnsHelloWorld = function(test) {
-	server.start(8080);
-	var request = http.get("http://localhost:8080");
-	request.on("response", function(response) {
-		var receivedData = false;
-		response.setEncoding("utf8");
+	var TEST_FILE = "generated/test/test.html";
 
-		test.equals(200, response.statusCode);
+	//Se llama cada vez que finaliza un test al llamar a test.done()
+	exports.tearDown = function(done) {
+		if (fs.existsSync(TEST_FILE)) {
+			fs.unlinkSync(TEST_FILE);
+			assert.ok(!fs.existsSync(TEST_FILE), "could not delete test file: [" + TEST_FILE + "]");
+		}
+		done();
+	};
 
-		response.on("data", function(chunk) {
-			receivedData = true;
-			test.equals("Hello World", chunk, "response text");
+	exports.test_serverServesHomePageFromFile = function(test) {
+		//Integration test, not a unit test
+		var testDir = "generated/test";
+		var testData = "This is served from a file";
+
+		fs.writeFileSync(TEST_FILE, testData);
+		httpGet("http://localhost:8080", function(response, responseData) {
+			test.equals(200, response.statusCode, "status code");
+			test.equals(testData, responseData, "response text");
+			test.done();
 		});
-		response.on("end", function() {
-			test.ok(receivedData, "should have received response data");
-			server.stop(function() {
-				test.done();
+	};
+
+	exports.test_serverReturns404ForEverythingExceptHomePage = function(test) {
+		//Integration test, not a unit test
+		httpGet("http://localhost:8080/somethingArbitrary", function(response, responseData) {
+			test.equals(404, response.statusCode, "status code");
+			test.done();
+		});
+	};
+
+	exports.test_serverReturnsHomePageWhenAskedForIndex = function(test) {
+		var testDir = "generated/test";
+		var testData = "This is served from a file";
+
+		fs.writeFileSync(TEST_FILE, testData);
+		httpGet("http://localhost:8080/index.html", function(response, responseData) {
+			test.equals(200, response.statusCode, "status code");
+			test.equals(testData, responseData, "response text");
+			test.done();
+		});
+	};
+
+	function httpGet(url, callback) {
+		server.start(TEST_FILE ,8080);
+		var request = http.get(url);
+		request.on("response", function(response) {
+			var receivedData = "";
+			response.setEncoding("utf8");
+
+			response.on("data", function(chunk) {
+				receivedData += chunk;
+			});
+
+			response.on("end", function() {
+				server.stop(function() {
+					callback(response, receivedData);
+				});
 			});
 		});
-	});
-};
+	}
 
-exports.test_serverServeAFile = function(test) {
-	//Integration test, not a unit test
-	var testDir = "generated/test";
-	var testFile = testDir + "/test.html";
-
-	fs.writeFileSync(testFile, "Hello world");
-
-	test.done();
-};
-
-exports.test_serverRequiresPortNumber = function(test) {
-	//Comprueba que la función lance un error, en este caso por no pasar portNumber, si vemos el método start de server.js veremos que lanza un new Error
-	test.throws(function() {
-		server.start();
-	});
-	test.done();
-};
-
-exports.test_serverRunsCallbackWhenStopCompletes = function(test) {
-	server.start(8080);
-	server.stop(function(error) {
-		test.done();	
-	});
-};
-
-exports.test_stopErrorsWhenNotRunning = function(test) {
-	server.stop(function(error) {
-		test.notEqual(error, undefined);
+	exports.test_serverRequiresFileToServe = function(test) {
+		test.throws(function() {
+			server.start();
+		});
 		test.done();
-	});
-};
+	};
+
+	exports.test_serverRequiresPortNumber = function(test) {
+		//Comprueba que la función lance un error, en este caso por no pasar portNumber, si vemos el método start de server.js veremos que lanza un new Error
+		test.throws(function() {
+			server.start(TEST_FILE);
+		});
+		test.done();
+	};
+
+	exports.test_serverRunsCallbackWhenStopCompletes = function(test) {
+		server.start(TEST_FILE, 8080);
+		server.stop(function(error) {
+		 	test.done();	
+		});
+	};
+
+	exports.test_stopErrorsWhenNotRunning = function(test) {
+		server.stop(function(error) {
+			test.notEqual(error, undefined);
+			test.done();
+		});
+	};
+}());
--- src/server/server.js
+++ src/server/server.js
@@ -1,14 +1,22 @@
 "use strict";
 
 var http = require("http");
+var fs = require("fs");
 var server;
 
-exports.start = function(portNumber) {
+exports.start = function(fileToServe, portNumber) {
 	if (!portNumber) throw new Error("Port number is required");
 	server = http.createServer();
 	server.on("request", function(request, response) {
-		response.statusCode = 200;
-		response.end("Hello World");
+		if (request.url === "/" || request.url === "/index.html") {
+			fs.readFile(fileToServe, function(err, data) {
+				if (err) throw err; //TODO: Fix me
+				response.end(data);	
+			});
+		} else {
+			response.statusCode = 404;
+			response.end();
+		}
 	});
 
 	server.listen(portNumber);
