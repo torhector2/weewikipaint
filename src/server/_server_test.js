f5133dcb6698c69f0db7d2e41258403463d452fd
torhector2 <torhector2@gmail.com>
2015-10-25 13:21:27 +0100

    We serve 404 page from file

--- src/server/_server_test.js
+++ src/server/_server_test.js
@@ -6,52 +6,91 @@
 	var fs = require("fs");
 	var assert = require("assert"); //Por cómo trabaja nodeunit no tiene asserts en setUp y teardown así que utilizamos otra librería
 
-	var TEST_FILE = "generated/test/test.html";
+	var TEST_HOME_PAGE = "generated/test/testHome.html";
+	var TEST_404_PAGE = "generated/test/test404.html";
 
 	//Se llama cada vez que finaliza un test al llamar a test.done()
 	exports.tearDown = function(done) {
-		if (fs.existsSync(TEST_FILE)) {
-			fs.unlinkSync(TEST_FILE);
-			assert.ok(!fs.existsSync(TEST_FILE), "could not delete test file: [" + TEST_FILE + "]");
-		}
+		cleanUpFile(TEST_HOME_PAGE);
+		cleanUpFile(TEST_404_PAGE);
+		
 		done();
 	};
 
-	exports.test_serverServesHomePageFromFile = function(test) {
+	exports.test_servesServesHomePageFromFile = function(test) {
 		//Integration test, not a unit test
-		var testDir = "generated/test";
-		var testData = "This is served from a file";
+		
+		var expectedData = "This is home page file";
+		fs.writeFileSync(TEST_HOME_PAGE, expectedData);
 
-		fs.writeFileSync(TEST_FILE, testData);
 		httpGet("http://localhost:8080", function(response, responseData) {
 			test.equals(200, response.statusCode, "status code");
-			test.equals(testData, responseData, "response text");
+			test.equals(expectedData, responseData, "response text");
 			test.done();
 		});
 	};
 
-	exports.test_serverReturns404ForEverythingExceptHomePage = function(test) {
+	exports.test_returns404FromFileForEverythingExceptHomePage = function(test) {
 		//Integration test, not a unit test
+
+		var expectedData = "This is 404 page file";
+		fs.writeFileSync(TEST_404_PAGE, expectedData);
+
 		httpGet("http://localhost:8080/somethingArbitrary", function(response, responseData) {
 			test.equals(404, response.statusCode, "status code");
+			test.equals(expectedData, responseData, "404 text");
 			test.done();
 		});
 	};
 
-	exports.test_serverReturnsHomePageWhenAskedForIndex = function(test) {
+	exports.test_returnsHomePageWhenAskedForIndex = function(test) {
 		var testDir = "generated/test";
-		var testData = "This is served from a file";
 
-		fs.writeFileSync(TEST_FILE, testData);
+		fs.writeFileSync(TEST_HOME_PAGE, "fooData");
 		httpGet("http://localhost:8080/index.html", function(response, responseData) {
 			test.equals(200, response.statusCode, "status code");
-			test.equals(testData, responseData, "response text");
+			test.done();
+		});
+	};
+
+	exports.test_requiresHomePageParameter = function(test) {
+		test.throws(function() {
+			server.start();
+		});
+		test.done();
+	};
+
+	exports.test_requires404PageParameter = function(test) {
+		test.throws(function() {
+			server.start(TEST_HOME_PAGE);
+		});
+		test.done();
+	};
+
+	exports.test_requiresPortNumber = function(test) {
+		//Comprueba que la función lance un error, en este caso por no pasar portNumber, si vemos el método start de server.js veremos que lanza un new Error
+		test.throws(function() {
+			server.start(TEST_HOME_PAGE, TEST_404_PAGE);
+		});
+		test.done();
+	};
+
+	exports.test_runsCallbackWhenStopCompletes = function(test) {
+		server.start(TEST_HOME_PAGE, TEST_404_PAGE, 8080);
+		server.stop(function(error) {
+		 	test.done();	
+		});
+	};
+
+	exports.test_stopSendErrorWhenNotRunning = function(test) {
+		server.stop(function(error) {
+			test.notEqual(error, undefined);
 			test.done();
 		});
 	};
 
 	function httpGet(url, callback) {
-		server.start(TEST_FILE ,8080);
+		server.start(TEST_HOME_PAGE, TEST_404_PAGE, 8080);
 		var request = http.get(url);
 		request.on("response", function(response) {
 			var receivedData = "";
@@ -69,32 +108,11 @@
 		});
 	}
 
-	exports.test_serverRequiresFileToServe = function(test) {
-		test.throws(function() {
-			server.start();
-		});
-		test.done();
-	};
+	function cleanUpFile(file) {
+		if (fs.existsSync(file)) {
+			fs.unlinkSync(file);
+			assert.ok(!fs.existsSync(file), "could not delete test file: [" + file + "]");
+		}
+	}
 
-	exports.test_serverRequiresPortNumber = function(test) {
-		//Comprueba que la función lance un error, en este caso por no pasar portNumber, si vemos el método start de server.js veremos que lanza un new Error
-		test.throws(function() {
-			server.start(TEST_FILE);
-		});
-		test.done();
-	};
-
-	exports.test_serverRunsCallbackWhenStopCompletes = function(test) {
-		server.start(TEST_FILE, 8080);
-		server.stop(function(error) {
-		 	test.done();	
-		});
-	};
-
-	exports.test_stopErrorsWhenNotRunning = function(test) {
-		server.stop(function(error) {
-			test.notEqual(error, undefined);
-			test.done();
-		});
-	};
 }());
